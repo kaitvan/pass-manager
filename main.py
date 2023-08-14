e7941e378cb864481a0695535314234e05a624e1
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-08-14 17:57:18 -0500

    Finish save button and generate password functionality

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    password_entry.delete(0, END)
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    num_letters = random.randint(8, 10)
+    num_symbols = random.randint(2, 4)
+    num_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for num in range(0, num_letters):
+        password_list.append(random.choice(letters))
+
+    for num in range(0, num_symbols):
+        password_list.append(random.choice(symbols))
+
+    for num in range(0, num_numbers):
+        password_list.append(str(random.choice(numbers)))
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    complete = len(website) != 0 and len(username) != 0 and len(password) != 0
+
+    if complete:
+        is_ok = messagebox.askokcancel(title=website, message=f"Username: {username}\n Password: {password}\n Does "
+                                                              f"this look right?")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {username} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+    else:
+        messagebox.showinfo(title="Incomplete", message="Please complete all fields.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -16,25 +67,29 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:", anchor="e", width=17)
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=40)
-website_input.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 username_label = Label(text="Email/Username:", anchor="e", width=17)
 username_label.grid(column=0, row=2)
 
-username_input = Entry(width=40)
-username_input.grid(column=1, row=2, columnspan=2)
+username_entry = Entry(width=40)
+username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(0, "kaitlyn@email.com")
+
 
 password_label = Label(text="Password:", anchor="e", width=17)
 password_label.grid(column=0, row=3)
 
-password_input = Entry(width=22)
-password_input.grid(column=1, row=3)
+password_entry = Entry(width=22)
+password_entry.grid(column=1, row=3)
 
-generate_password_btn = Button(text="Generate Password")
+generate_password_btn = Button(text="Generate Password", command=generate)
 generate_password_btn.grid(column=2, row=3)
 
-add_btn = Button(text="Add", width=38)
+add_btn = Button(text="Add", width=38, command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
+
 
 window.mainloop()
