5a6c2231433b05390050617017b25d07cd0ad57b
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-08-14 18:13:46 -0500

    Import pyperclip and copy the randomly generated password when the user clicks the generate button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -21,7 +22,9 @@ def generate_password():
     shuffle(password_list)
     password = "".join(password_list)
 
+    pyperclip.copy(password)
     password_entry.insert(0, password)
+
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
